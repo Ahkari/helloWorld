2612706ce18197661ded451573e79671614e2f96
Ahkari <gqz825584053@qq.com>
2015-04-07 00:40:07 +0800

    给出部分排序算法的js实现

--- dataStructuresInC/data.js
+++ dataStructuresInC/data.js
@@ -126,27 +126,124 @@ console.log(indexChuan(strMain,strChild));
 
 //哈希表
 
-//插入排序
+//内部排序
+//Q1：直接插入排序。
+//将一个记录插入到已排好序的有序表中。得到一个新的，记录数增1的有序表。
+function InsertSort(arr){
+	var len=arr.length;
+	var arrResult=[arr[0]];
+	for (var i=1;i<len;i++){
+		for (var j=0;j<i+1;j++){
+			if (arr[i]<=arrResult[j]){
+				arrResult.splice(j,0,arr[i]);
+				break;
+			}
+		}
+		if (arrResult.length!=i+1){
+			arrResult.push(arr[i]);
+		}
+	}
+	return arrResult;
+}
+var array=[49,38,65,97,76,13,27,49];
+var result=InsertSort(array);
+console.log(result);
 
-//折半插入排序
+//Q2：折半插入排序
+//相对于简单插入，插入查找时从中间开始
+function BInsertSort(arr){
+	var len=arr.length;
+	var arrResult=[arr[0]];
+	for (var i=1;i<len;i++){
+		var mid=Math.floor((1+arrResult.length)/2);
+		var midValue=arrResult[mid-1];
+		if (midValue>=arr[i]){
+			for (var j=0;j<mid+1;j++){
+				if (arr[i]<=arrResult[j]){
+					arrResult.splice(j,0,arr[i]);
+					break;
+				}
+			}
+		}else if(midValue<arr[i]){
+			for (var j=mid+1;j<i+1;j++){
+				if (arr[i]<=arrResult[j]){
+					arrResult.splice(j,0,arr[i]);
+					break;
+				}
+			}
 
-//2-路插入排序
+		};
+		if (arrResult.length!=i+1){
+			arrResult.push(arr[i]);
+		}
+	}
+	return arrResult;
+}
+var array=[49,38,65,97,76,13,27,49];
+var result=BInsertSort(array);
+console.log(result);
+
+//Q3：2-路插入排序
+function twoInsert(arr){
+	var len=arr.length;
+	var resultArr=[];
+	resultArr.push(arr[0]);
+	for (var i=1;i<len;i++){
+		resultArr.push(arr[i]);
+		for (var j=0;j<i;j++){
+			if (resultArr[i-j]<=resultArr[i-j-1]){
+				var resultTmp=resultArr[i-j-1];
+				resultArr[i-j-1]=resultArr[i-j];
+				resultArr[i-j]=resultTmp;
+			}
+		}
+	}
+  return resultArr;
+}
+var array=[49,38,65,97,76,13,27,49];
+var result=twoInsert(array);
+console.log(result);
 
 //希尔排序
+//将带排序记录序列分割成若干子序列插入排序，多次后基本有序时，再对全体记录进行一次直接插入排序。
+//需要Q1，2，3中的方法。辅以增量算法。
 
-//冒泡排序
+//Q4：冒泡排序
+function bobbleSort(arr){
+	var len=arr.length;
+	for (var i=0;i<len;i++){
+		for (var j=0;j<(len-i-1);j++){
+			if (arr[j]<arr[j+1]){
+			}else if(arr[j]>=arr[j+1]){
+				var arrTmp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=arrTmp;
+			}
+		}
+	}
+	return arr;
+}
+var array=[49,38,65,97,76,13,27,49];
+var result=bobbleSort(array);
+console.log(result);
 
-//快速排序
+//Q5：快速排序
+//一趟排序将待排记录分割成独立两部分，然后在对两部分分别继续排序以达整个序列有序。
 
-//选择排序
+//Q6：简单选择排序
+//每一趟在n-i+1(i=1,2,...,n-1)个记录中选取关键字最小的记录作为有序序列中第i个记录。
 
-//树形选择排序
+//Q7：树形选择排序
+//n个记录关键字两两比较，直至选出最小关键字的记录。（特性：选出最小值后把他变为最大值再次树形排序可得次大值）
 
-//堆排序
+//Q8：堆排序
+//一个堆是完全二叉树，父节点一定比子节点大或小，移除根节点后把末节点提上去，然后慢慢换下去。就可得新堆。
 
-//归并排序s
+//Q9：归并排序
+//两个或两个以上的有序表组合成一个新的有序表。从数组最开始两两归并。
 
-//基数排序
+//Q10：基数排序
+//
 
 
 
