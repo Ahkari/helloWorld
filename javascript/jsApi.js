37c82ea4d84b72bd760d07f445dc9268ccabe153
Ahkari <gqz825584053@qq.com>
2015-05-15 17:12:54 +0800

    CSS示例

---
+++ javascript/jsApi.js
@@ -0,0 +1,309 @@
+/*
+javascript方法
+ */
+
+
+Math.abs(number)
+
+Math.acos(number)
+
+strVariable.anchor(anchorStirng)
+
+apply([thisObj[,argArray]]) //
+
+Math.asin(number)
+
+Math.atan(number)
+
+Math.atan2(y,x) //返回由X轴到(y,x)点的角度(弧度单位)
+
+myEnum.atEnd()  //当前枚举项是否位于集合的末尾,boolean
+
+strVariable.big()
+
+strVariable.blink()
+
+strVariable.bold()
+
+call([thisObj[,arg1[,arg2[,...[,argN]]]]])  //
+
+Math.ceil(number)   //返回大于等于其数字参数的最小整数
+
+strObj.charAt(index)    //返回指定索引处的字符
+
+strObj.charCodeAt(index)    //返回指定位置上字符的Unicode编码
+
+rgExp.compile(patten,[flags])   //编译正则表达式,使其执行的更快
+
+array1.concat([item[,item2[,...[,itemN]]]]) //多个数组组合成新数组
+
+string1.concat([string1[,string2[,...[,stringN]]]]) //多个字符串连接成新字符串
+
+Math.cos(number)
+
+decodeURL(URIstring)    //
+
+decodeURLComponent(encodeURIString)
+
+array.dimensions()
+
+encodeURL(URIString)    //
+
+encodeURIComponent(encodedURIString)
+
+escape(charString)
+
+eval(codeString)    //检查js代码并执行
+
+rgExp.exec(str) // 
+
+Math.exp(number)
+
+strVariable.fixed()
+
+Math.floor(number)  //
+
+strVariable.fontcolor(colorVal)
+
+strVariable.fontsize(intSize)
+
+String.fromCharCode([code1[,code2[,...[,codeN]]]])
+
+dateObj.getDate()   //
+
+dateObj.getDay()    //一周中的日期值(日0~六6)
+
+dateObj.getFullYear()   //
+
+dateObj.getHours()  //
+
+safeArray.getItem(dimension1[,dimension2,...],dimensionN)
+
+dateObj.getMilliseconds()   //本地时间的毫秒值
+
+dateObj.getMinutes()    //
+
+dateObj.getMonth()  //
+
+dateObj.getSeconds()    //
+
+dateObj.getTime()   //该整数代表1970.1.1到现在的毫秒数
+
+dateObj.getTimezoneOffset()
+
+dateObj.getUTCDate()
+
+dateObj.getUTCDay()
+
+dateObj.getUTCFullYear()
+
+dateObj.getUTCHours()
+
+dateObj.getUTCMilliseconds()
+
+dateObj.getUTCMinutes()
+
+dateObj.getUTCMonth()
+
+dataObj.getUTCSeconds()
+
+dateObj.getVarDate()
+
+dateObj.getYear()   //
+
+strObj.indexOf(subString[,startIndex])  //查找substring所在位置,从startIndex处开始
+
+isFinite(number)    //number是否是NaN,负无穷或正无穷
+
+isNaN(numValue) //
+
+strVariable.italics()
+
+enumObj.item()  //返回集合中的当前项
+
+arrayObj.join(separator)    //返回数组成员用separate链接起来的字符串结果
+
+strObj.lastIndexOf(subString[,startIndex])  //返回字符对象中子字符串最后出现的位置
+
+safeArray.lbound(dimension)
+
+strVariable.link(linkstring)
+
+stringVar.localeCompare(stringExp)
+
+Math.log(number)
+
+stringObj.match(reExp)  //  
+
+Math.max([number1[,number2[...[,numberN]]]])    //给出多个数值表达式中较大者
+
+Math.min([number1[,number2[...[,numberN]]]])    //...较小者
+
+enumObj.moveFirst() //将集合当前项设置为第一项
+
+enumObj.moveNext()  //集合中当前项移动到下一项
+
+Date.parse(dateVal) //返回自1970.1.1以来的毫秒数
+
+parseFloat(numString)   //返回字符串转换得到的浮点数
+
+parseInt(numString,[radix]) //
+
+arrayObj.pop()  //
+
+Math.pow(base,exponent)
+
+arrayObj.push([item1[,item2[,...[,itemN]]]])    //
+
+Math.random()   //
+
+stringObj.replace(rgExp,replaceText)    //
+
+arrayObj.reverse()  //数组反序
+
+Math.round(number)  //返回与表达式最接近的整数
+
+stringObj.search(rgExp) //返回与正则表达式查找内容匹配的第一个子字符串的位置
+
+dateObj.setDate(number)
+
+dateObj.setFullYear(numYear[,numMonth[,numDate]])
+
+dateObj.setHours(numHours[,numMin[,numSec[,numMilli]]])
+
+dateObj.setMilliseconds(numMilli)
+
+dateObj.setMinutes(numMinutes[,numSeconds[,numMilli]])
+
+dateObj.setMonth(numMonth[,dateVal])
+
+dateObj.setSeconds(numSeconds[,numMilli])
+
+dateObj.setTime(setMilliseconds)
+
+dateObj.setUTCDate(numDate)
+
+dateObj.setUTCFullYear(numYear[,numMonth[,numDate]])
+
+dateObj.setUTCHours(numHours[,numMin[,numSec[,numMilli]]])
+
+dateObj.setUTCMilliseconds(numMilli)
+
+dateObj.setUTCMinutes(numMinutes[,numSeconds[,numMilli]])
+
+dateObj.setUTCMonth(numMonth[,dateVal])
+
+dateObj.setUTCSeconds(numSeconds[,numMilli])
+
+dateObj.setYear(numYear)
+
+arrayObj.shift()    //
+
+Math.sin(number)
+
+arrayObj.slice(start,[end]) //返回start到end处(不包含end)的一段数组
+
+stringObj.slice(start,[end])    //    
+
+strVariable.small()
+
+arrayObj.sort(sortfunction)  //用sortfunction排序
+
+arrayObj.splice(start,deleteCount,[item1[,item2[,...[,itemN]]]])    //
+
+stringObj.split([separator[,limit]])   //按照separator分割字符串,返回limit个
+
+Math.sqrt(number)   
+
+strVariable.strike()
+
+strVariable.sub()
+
+stringVar.substr(start[,length])    //从指定位置开始的定长子字符串
+
+strVariable.substring(start,end)
+
+strVariable.sup()
+
+Math.tan(number)
+
+rgExp.text(str) //被查找的字符串中是否存在,boolean
+
+safeArray.toArray()
+
+objDate.toDateString()
+
+numObj.toExponential([fractionDigits])
+
+numObj.toFixed([fractionDigits])
+
+dateObj.toGMTString()
+
+objDate.toLocaleDateString()
+
+stringVar.toLocaleLowerCase()
+
+dateObj.toLocaleString()
+
+objDate.toLocaleTimeString()
+
+stringVar.tolocaleUpperCase()   //转为大写,同时适应宿主环境当前区域设置
+
+strVariable.toLowerCase()   //转为小写
+
+numObj.toPrecision([Precision])
+
+objectname.toString([radix])    //
+
+objDate.toTimeString()
+
+strVariable.toUpperCase()   //
+
+dateObj.toUTCString()
+
+safeArray.ubound(dimension)
+
+unescape(charstring)
+
+arrayObj.unshift([item1[,item2[,...[,itemN]]]]) //
+
+Date.UTC(year,month.day[,hours[,minutes[,seconds[,ms]]]])
+
+object.valueOf()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
